1c8049b60ad7cdee53898c3cf3038c2edccfe2c5
MAMS3 <marco.alexander25.mams@gmail.com>
2023-01-31 18:02:21 -0500

    TYPESCRIPT
    INTRODUCCION Y CONFIGURACION DE TYPESCRIPT

---
+++ TS/code.ts
@@ -0,0 +1,15 @@
+let texto:string ="lorem ipsum dolor its amet consecutive"
+let numero: number = 45
+let siOno: boolean =false;
+
+
+var cualquierCosa:any = false
+
+let arregloVariado:any = [] = ["juan", 2 , false]
+let arregloDeTextos: string[] = ["juan"]; 
+const pi:string = "3-1416"
+
+let arregloDeNumeros:number[] = [1,2,3,4]
+let arrayEstructurado:[boolean,string,string,number] = [true,"texto","texto",27]
+let arrayEstructuradoDeArrays:[boolean,string,string,number][] = [[true,"texto","texto",27]]
+
