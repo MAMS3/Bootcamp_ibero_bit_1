a2171261be5ac954261487e91285aa2ff8079807
MAMS3 <marco.alexander25.mams@gmail.com>
2023-01-31 18:02:35 -0500

    API

--- API/assets/js/code.js
+++
@@ -1,8 +0,0 @@
-let urlAPI = "https://api.openbrewerydb.org/breweries"
-
-let enero = fetch(urlAPI)
-    // console.log(enero)
-enero.then(res => res.json())
-    .then((info) => {
-        console.log(info)
-    })
